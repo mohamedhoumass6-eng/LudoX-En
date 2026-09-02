c3615c9315181af0886584735f71418bd293f289
mohamedhoumass6-eng <mohamedhoumass6@gmail.com>
2026-09-02 13:26:56 +0100

    Create LudoX En app

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>LudoX En</h1>
+      <p>Welcome to LudoX En 🎮</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
